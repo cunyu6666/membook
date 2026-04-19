20caec75214a60b2fb8a9df4dbf56369b54aaa0f
寸雨 <minghuazzz@qq.com>
2026-04-19 18:32:58 +0800

    chore: 更新 TypeScript 和 Vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,16 @@
  */
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
   server: {
     proxy: {
       "/api": "http://localhost:8787",
